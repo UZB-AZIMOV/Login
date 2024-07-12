0a7cfe3a732062264c6932b2249786814808f8ac
UZB-AZIMO <abduvorisazimov712@gmail.com>
2024-07-12 10:11:16 +0500

    df

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert ("ASSALOMU ALEKUM")
+alert ("🖐 ASSALOMU ALEKUM 🖐")
 import { USERS } from "../db/server.js"
 const btnOpen = document.querySelector(".btn__open")
 const btnClose = document.querySelector(".btn__close")
